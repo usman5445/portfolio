ee00f3c149aecd6f9ff06e08431ffb16ecd6c1e3
Usman <usmangavandi5445@gmail.com>
2023-04-16 21:26:48 +0530

    eduaction page added

--- src/Pages/Education.jsx
+++ src/Pages/Education.jsx
@@ -6,29 +6,20 @@ export default function Education() {
   const typedRef = useRef();
   const containerRef = useRef();
   useEffect(() => {
-    const typed = new Typed(typedRef.current, {
-      strings: ["Frontend Developer", "Backend Developer", "UI Designer"],
-      typeSpeed: 50,
-      backSpeed: 50,
-      smartBackspace: 100,
-      loop: true,
-    });
     VanillaTilt.init(containerRef.current, {
       max: 2,
       glare: true,
       "max-glare": "0.5",
     });
-    return () => {
-      typed.destroy();
-    };
+    return () => {};
   }, []);
   return (
     <div
       ref={containerRef}
-      className=" relative grid h-4/5 w-4/5 grid-cols-2 content-center items-center rounded-2xl border-l-2 border-t-2 border-white/60  bg-white/20 px-8 shadow-2xl backdrop-blur-2xl "
+      className="  relative grid h-4/5 w-4/5 grid-cols-2 content-center rounded-2xl border-l-2 border-t-2 border-white/60  bg-white/20 px-8 shadow-2xl backdrop-blur-2xl "
     >
       <div className="absolute top-1 flex w-full items-center justify-between px-8">
-        <p>Education.md</p>
+        <p>Education_&_Certificates.md</p>
         <div
           className="flex space-x-2 
         "
@@ -38,18 +29,26 @@ export default function Education() {
           <div className="h-4 w-4 rounded-full bg-white"></div>
         </div>
       </div>
-      <img src={myPhoto} alt="Usman Photo" className="h-5/6 " />
-      <div>
-        <h2 className="text-8xl">Hi There!</h2>
-        <h1 className="text-8xl">
-          I am <span className="font-bold">Usman.</span>
-        </h1>
-        <h1 className="text-8xl">
-          a <span className="font-bold text-yellow-300" ref={typedRef}></span>.
-        </h1>
-        <h1 className="absolute bottom-4 right-4 text-6xl font-semibold text-white/10">
-          #TechGeek.
-        </h1>
+      <div className=" col-span-2 grid grid-cols-2 divide-x-2 py-16   text-center  text-5xl leading-relaxed">
+        <div className="">
+          <h1>10th (SSC)</h1>
+          <h1 className="font-bold">73.40%</h1>
+          <h1>Maharashtra State Board</h1>
+        </div>
+
+        <div>
+          <h1>12th (HSC)</h1>
+          <h1 className="font-bold">68%</h1>
+          <h1>Maharashtra State Board</h1>
+        </div>
+      </div>
+      <div className="col-span-2 space-y-2 py-16 text-center">
+        <h1 className="text-4xl font-bold">Frontend Development Bootcamp</h1>
+        <h3 className="font-medium">By RELEVEL (UNACADEMY)</h3>
+        <h4 className="font-extralight">DECEMBER 2021 - JUNE 2022 </h4>
+        <button className="rounded-full border border-white px-2 py-1 text-2xl hover:bg-white hover:text-black">
+          View Certificate
+        </button>
       </div>
     </div>
   );
